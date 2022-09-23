91c737587dad33670f4e96006d3caa85fd4c11e8
Luca Georges Francois <luca@quartz.technology>
2022-09-22 19:09:00 -0700

    feat: add combinations building and main optimizer logic
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod optimizer;
+
+use crate::optimizer::{build_combinations, find_optimization};
+
 fn main() {
+    let suffix_length = 4;
+    let combinations = build_combinations(suffix_length);
+
+    find_optimization("myFunction_", "(address)", &combinations, 3);
     println!("Hello, world!");
 }
