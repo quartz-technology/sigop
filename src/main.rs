de40219e2fa88541faadf4e91f73b304c58ca5e4
Luca G.F <luca@quartz.technology>
2022-09-22 21:03:36 -0700

    feat: add cli and progress bar (#8)
    * feat: add clap cli
    
    * feat: add progress bar for debug view
    
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,31 @@ mod preprocessor;
 
 use crate::optimizer::{build_combinations, find_optimization};
 use crate::preprocessor::{try_preprocess, PreProcessError};
+use clap::Parser;
 use env_logger::Builder;
 use error_stack::Report;
 use log::{error, LevelFilter};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// The function signature to optimize.
+    #[clap(short, long)]
+    signature: String,
+
+    /// The maximum size of the suffix following the original function name.
+    #[clap(short, long, default_value_t = 3)]
+    length: u8,
+
+    /// The number of zero-bytes you want to have at the beginning of the optimized function.
+    #[clap(short, long, default_value_t = 2)]
+    target: u8,
+}
 
 fn run(
     function_signature: &str,
     suffix_length: u8,
     optimization_target: u8,
-    _debug: bool,
 ) -> Result<(), Report<PreProcessError>> {
     let mut function = try_preprocess(function_signature)?;
     let combinations = build_combinations(suffix_length);
@@ -21,6 +37,7 @@ fn run(
         function.name.as_str(),
         function.params.as_str(),
         &combinations,
+        suffix_length,
         optimization_target,
     );
 
@@ -34,16 +51,9 @@ fn main() {
     builder.parse_default_env();
     builder.init();
 
-    let function_signature = "myFunction(address)";
-    let suffix_length = 4;
-    let optimization_target = 3;
+    let cli = Cli::parse();
 
-    match run(
-        function_signature,
-        suffix_length,
-        optimization_target,
-        false,
-    ) {
+    match run(cli.signature.as_str(), cli.length, cli.target) {
         Ok(_) => {}
         Err(err) => match err.current_context() {
             PreProcessError::InvalidFunctionSignatureParenthesis(msg) => {
