b4d35c846b5e5c356ad3a49578cf3fdf2aadaa52
Luca Georges Francois <luca@quartz.technology>
2022-09-22 20:34:31 -0700

    feat: add clap cli
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,30 @@ mod preprocessor;
 
 use crate::optimizer::{build_combinations, find_optimization};
 use crate::preprocessor::{try_preprocess, PreProcessError};
+use clap::Parser;
 use env_logger::Builder;
 use error_stack::Report;
 use log::{error, LevelFilter};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// The function signature to optimize.
+    #[clap(short, long)]
+    signature: String,
+
+    /// The maximum size of the suffix following the original function name.
+    #[clap(short, long, default_value_t = 3)]
+    length: u8,
+
+    /// The number of zero-bytes you want to have at the beginning of the optimized function.
+    #[clap(short, long, default_value_t = 2)]
+    target: u8,
+
+    /// Prints a progression bar for iterations.
+    #[clap(short, long, action)]
+    debug: bool,
+}
 
 fn run(
     function_signature: &str,
@@ -34,16 +55,9 @@ fn main() {
     builder.parse_default_env();
     builder.init();
 
-    let function_signature = "myFunction(address)";
-    let suffix_length = 4;
-    let optimization_target = 3;
+    let cli = Cli::parse();
 
-    match run(
-        function_signature,
-        suffix_length,
-        optimization_target,
-        false,
-    ) {
+    match run(cli.signature.as_str(), cli.length, cli.target, cli.debug) {
         Ok(_) => {}
         Err(err) => match err.current_context() {
             PreProcessError::InvalidFunctionSignatureParenthesis(msg) => {
